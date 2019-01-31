2f1dddd2e41ddc219690aad7dd8161c20499e233
Szabadi Péter <kreativ.szabadi@gmail.com>
2019-01-31 11:50:38 +0100

    add routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { UserListComponent } from './users/user-list/user-list.component';
+import { UserEditCreateComponent } from './users/user-edit-create/user-edit-create.component';
+import { QuestionListComponent } from './questions/question-list/question-list.component';
+import { QuestionEditCreateComponent } from './questions/question-edit-create/question-edit-create.component';
+import { TestListComponent } from './tests/test-list/test-list.component';
+import { TestEditCreateComponent } from './tests/test-edit-create/test-edit-create.component';
+import { ResultListComponent } from './results/result-list/result-list.component';
+import { ResultEditCreateComponent } from './results/result-edit-create/result-edit-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'users/list', component: UserListComponent },
+  { path: 'users/edit', component: UserEditCreateComponent },
+  { path: 'users/edit/:id', component: UserEditCreateComponent },
+  { path: 'questions/list', component: QuestionListComponent },
+  { path: 'questions/edit', component: QuestionEditCreateComponent },
+  { path: 'questions/edit/:id', component: QuestionEditCreateComponent },
+  { path: 'tests/list', component: TestListComponent },
+  { path: 'tests/edit', component: TestEditCreateComponent },
+  { path: 'tests/edit/:id', component: TestEditCreateComponent },
+  { path: 'results/list', component: ResultListComponent },
+  { path: 'results/edit', component: ResultEditCreateComponent },
+  { path: 'results/edit/:id', component: ResultEditCreateComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
