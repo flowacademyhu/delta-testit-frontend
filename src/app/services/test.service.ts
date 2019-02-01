dabb3624e54eb71457f0526555d6cd4fb118f273
Szabadi Péter <kreativ.szabadi@gmail.com>
2019-02-01 15:23:38 +0100

    add new test form

---
+++ src/app/services/test.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TestModel } from '../models/test.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TestService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.httpClient.get('http://localhost:8080/tests');
+  }
+
+  getTest(id: number) {
+    return this.httpClient.get('http://localhost:8080/tests/' + id);
+  }
+
+  createTest(test: TestModel): Observable<any> {
+    return this.httpClient.post('http://localhost:8080/tests', test);
+  }
+
+  editTest(test: TestModel): Observable<any> {
+    return this.httpClient.put('http://localhost:8080/tests' + test.id, test);
+  }
+
+  deleteTest(id: number) {
+    return this.httpClient.delete('http://localhost:8080/test/' + id);
+  }
+
+}
