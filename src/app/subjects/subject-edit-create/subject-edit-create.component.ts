05b8f18a7981f7b24fc2469efa9dd7307238a9b1
vrgbrg <vrgbrg@gmail.com>
2019-02-11 09:58:31 +0100

    add subject component

---
+++ src/app/subjects/subject-edit-create/subject-edit-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subject-edit-create',
+  templateUrl: './subject-edit-create.component.html',
+  styleUrls: ['./subject-edit-create.component.scss']
+})
+export class SubjectEditCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
