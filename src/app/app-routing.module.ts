9017547473a4de35a6dfaa092275f593bdbdd99f
vrgbrg <vrgbrg@gmail.com>
2019-02-12 11:59:45 +0100

    update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,6 @@ import { QuestionListComponent } from './questions/question-list/question-list.c
 import { QuestionEditCreateComponent } from './questions/question-edit-create/question-edit-create.component';
 import { TestListComponent } from './tests/test-list/test-list.component';
 import { TestEditCreateComponent } from './tests/test-edit-create/test-edit-create.component';
-import { ResultListComponent } from './results/result-list/result-list.component';
-import { ResultEditCreateComponent } from './results/result-edit-create/result-edit-create.component';
 import { LoginComponent } from './login/login.component';
 import { LoginLayoutComponent } from './layouts/login-layout.component';
 import { HomeComponent } from './home/home.component';
@@ -68,18 +66,6 @@ const routes: Routes = [
       {
         path: 'tests/edit/:id',
         component: TestEditCreateComponent
-      },
-      {
-        path: 'results/list',
-        component: ResultListComponent
-      },
-      {
-        path: 'results/edit',
-        component: ResultEditCreateComponent
-      },
-      {
-        path: 'user/results/edit/:id',
-        component: ResultEditCreateComponent
       }
     ]
   },
@@ -93,7 +79,7 @@ const routes: Routes = [
       }
     ]
   },
-  { path: '**', redirectTo: '' },  
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
